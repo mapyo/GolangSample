81a150d8f1c1e4d59e0a2d10fa1386858e42bb54
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-06-15 07:31:01 +0900

    Add encode sample

--- polyline/main.go
+++ polyline/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"github.com/paulmach/go.geo"
+	"fmt"
 )
 
 func main() {
-	// decode
+	decode()
+	encode()
+}
+
+func encode() {
+	path := geo.Path{
+		PointSet: geo.PointSet{
+			geo.Point{139.760820, 35.678340},
+			geo.Point{139.759750, 35.675270},
+		},
+	}
+
+	polyline := path.Encode()
+	fmt.Println(polyline)
+}
+
+func decode() {
 	path := geo.NewPathFromEncoding("slwxEc``tYdRtEsM|f@dBvV}LnMgBlIsN`GyHr@cY_FaGqNL}LiBwM{DoLiCmMbAkW`BmH`GoLdYtE")
 
 	fmt.Println(path)
