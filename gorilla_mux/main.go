9db9733920139ef3a08a473a6381e596829ea7b0
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-06-18 01:25:22 +0900

    fix gorilla_mux sample

--- gorilla_mux/main.go
+++ gorilla_mux/main.go
@@ -7,13 +7,32 @@ import (
 	"log"
 )
 
+var DB = map[string]string{
+	"user1": "foo",
+	"user2": "bar",
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
+	r.HandleFunc("/user/{name}", userHandler).Methods(http.MethodGet)
+
 	log.Fatal(http.ListenAndServe(":8081", r))
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	user := mux.Vars(r)["name"]
+
+	value, ok := DB[user]
+	if ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, value)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "mux sample")
+	fmt.Fprintf(w, "pong")
 }
