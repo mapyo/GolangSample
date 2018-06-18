04a133affffe752806b4ba31e4b8124a9084a5da
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-06-19 00:13:02 +0900

    Add mux basic auth sample

--- gorilla_mux/main.go
+++ gorilla_mux/main.go
@@ -12,12 +12,37 @@ var DB = map[string]string{
 	"user2": "bar",
 }
 
+var secrets = map[string]string{
+	"foo":  "secret1",
+	"manu": "seecret2",
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
 	r.HandleFunc("/user/{name}", userHandler).Methods(http.MethodGet)
+	r.HandleFunc("/", basicHandler).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe(":8081", r))
+	log.Fatal(http.ListenAndServe(":8082", r))
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	user, pass, ok := r.BasicAuth()
+	if !ok || !existsSecretUser(user, pass) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "user: %s, secret: %s", user, pass)
+}
+
+func existsSecretUser(user string, pass string) bool {
+	if _, ok := secrets[user]; ok {
+		return true
+	} else {
+		return false
+	}
 }
 
 func userHandler(w http.ResponseWriter, r *http.Request) {
