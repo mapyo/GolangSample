1435d3b1b51808541cde06ec1418379aaf7c3932
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-07-05 10:02:55 +0900

    wip

--- elasticsearch/src/main.go
+++ elasticsearch/src/main.go
@@ -2,9 +2,30 @@ package main // import "github.com/mapyo/GolangSample/elasticsearch"
 
 import (
 	"github.com/urfave/cli"
-	"github.com/olivere/elastic"
+	"os"
+	"fmt"
 )
 
 func main() {
+	app := cli.NewApp()
 
+	app.Name = "elasticsearch sample"
+	app.Usage = "elasticsearch sample command"
+	app.Version = "0.0.1"
+	app.Commands = commands
+	app.Run(os.Args)
+}
+
+var commands = []cli.Command{
+	createIndexAndMappingCommand,
+}
+
+var createIndexAndMappingCommand = cli.Command{
+	Name:   "create",
+	Usage:  "create index and mapping",
+	Action: createIndexAndMapping,
+}
+
+func createIndexAndMapping(c *cli.Context) {
+	fmt.Println("hoge")
 }
