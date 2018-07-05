9d40961ae29004f9bea9b42e24d9aa2d96e4ab6c
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-07-05 09:27:09 +0900

    vgo

---
+++ elasticsearch/go.mod
@@ -0,0 +1,6 @@
+module github.com/mapyo/GolangSample
+
+require (
+	github.com/olivere/elastic v1.0.1
+	github.com/urfave/cli v1.20.0
+)
---
+++ elasticsearch/src/main.go
@@ -0,0 +1,10 @@
+package main // import "github.com/mapyo/GolangSample/elasticsearch"
+
+import (
+	"github.com/urfave/cli"
+	"github.com/olivere/elastic"
+)
+
+func main() {
+
+}
