964da9010aa267d8826814eb48891a4a7ba02f06
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-07-13 00:37:25 +0900

    create elasticsearch index

--- elasticsearch/go.mod
+++ elasticsearch/go.mod
@@ -1,6 +1,8 @@
 module github.com/mapyo/GolangSample/elasticsearch
 
 require (
+	github.com/mailru/easyjson v0.0.0-20180606163543-3fdea8d05856
 	github.com/olivere/elastic v0.0.0-20180626153000-d06c9338b70f
+	github.com/pkg/errors v0.8.0
 	github.com/urfave/cli v1.20.0
 )
--- elasticsearch/src/command/create_index_and_mapping.go
+++ elasticsearch/src/command/create_index_and_mapping.go
@@ -2,6 +2,8 @@ package command
 
 import (
 	"github.com/urfave/cli"
+	"github.com/olivere/elastic"
+	"context"
 	"fmt"
 )
 
@@ -12,5 +14,46 @@ var createIndexAndMappingCommand = cli.Command{
 }
 
 func createIndexAndMapping(c *cli.Context) {
-	fmt.Println("createIndexAndMapping!!!")
+	client, err := elastic.NewSimpleClient()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	defer client.Stop()
+
+	mapping := `
+{
+ "settings":{
+   "number_of_shards":1,
+   "number_of_replicas":0
+ },
+ "mappings": {
+   "type": {
+     "properties": {
+       "name": {
+         "type": "keyword"
+       },
+       "location": {
+         "type": "geo_point"
+       }
+     }
+   }
+ }
+}
+`
+
+	ctx := context.Background()
+	createIndex, err := client.CreateIndex("mapyo").BodyString(mapping).Do(ctx)
+	fmt.Println(createIndex)
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	if !createIndex.Acknowledged {
+		fmt.Println("Not acknowledged")
+	} else {
+		fmt.Println("success")
+	}
 }
